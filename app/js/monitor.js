11e69685d6991888106b0f3898e543e4b61a67f1
Aleksey Belchenkov <u608110@gmail.com>
2020-06-19 12:53:49 +0300

    Output Static System Stats

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,14 @@ const os = osu.os;
 const cpu = osu.cpu;
 const mem = osu.mem;
 
+// Run every 2 seconds
+setInterval(() => {
+    // CPU Usage
+    cpu.usage().then(info => {
+        document.getElementById('cpu-usage').innerText = info + '%';
+    });
+}, 2000);
+
 // Set model
 document.getElementById('cpu-model').innerText = cpu.model();
 // Computer Name
