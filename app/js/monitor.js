dacc84f3c4219bf4b2a7bcb5f188155e30b76bef
Aleksey Belchenkov <u608110@gmail.com>
2020-06-19 12:51:46 +0300

    Output Static System Stats

---
+++ app/js/monitor.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const osu = require('node-os-utils');
+const os = osu.os;
+const cpu = osu.cpu;
+const mem = osu.mem;
+
+// Set model
+document.getElementById('cpu-model').innerText = cpu.model();
+// Computer Name
+document.getElementById('comp-name').innerText = os.hostname();
+// OS
+document.getElementById('os').innerText = `${os.type()} ${os.arch()}`
+// Total Mem
+mem.info().then(info => {
+   document.getElementById('mem-total').innerText = info.totalMemMb;
+});
